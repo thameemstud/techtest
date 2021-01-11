4ba4d7d8fd5267be89c0a5930707feb5a394e52d
thameemstud <thameemstud@gmail.com>
2021-01-11 13:37:28 +0400

    Update forms.py
    sdfsd

--- directory/forms.py
+++ directory/forms.py
@@ -4,6 +4,15 @@ from .models import Teacher,Subject
 from django.core.validators import FileExtensionValidator
 
 
+class FilterForm(forms.Form):
+    filter_lastname = forms.CharField(required=False)
+    filter_subject = forms.CharField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'textinput textInput form-control'
+
 class BulkUploadForm(forms.Form):
 
     csv_file = forms.FileField(
@@ -14,17 +23,3 @@ class BulkUploadForm(forms.Form):
             )
 
 
-
-
-# Create the form class.
-# class TeacherForm(ModelForm):
-#     subject = ModelMultipleChoiceField(widget=CheckboxSelectMultiple(), queryset=Subject.objects.all())
-#     class Meta:
-#         model = Teacher
-#         fields = ['firstName', 'lastName', 'profilePicture', 'email', 'phone', 'roomNumber']
-
-#     def clean_subject(self):
-#         b = self.cleaned_data['subject']
-#         if b.count() > 5:
-#             raise ValidationError("Cannot assign more than 5 subject to a teacher.")
-#         return self.cleaned_data['subject']
